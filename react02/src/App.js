0e99a5b3f65fa5257e163d2169e3a6856e7c24f3
kimtaekyung <nerobong2@gmail.com>
2019-03-20 16:34:28 +0900

    .

---
+++ react02/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Test from './component/Test';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Test></Test>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react02/src/component/Test.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../style/test.scss';
+
+class Test extends Component {
+    render() {
+        return (
+            <div className='a'>
+                <div className='b'>테스트</div>
+            </div>
+        );
+    }
+}
+
+export default Test;
